68111068d884de80c27bad4abab735c468d5d8ae
juujulio <julioeduardon9@hotmail.com>
2023-08-03 20:22:57 -0300

    Create new commands
     - Added the structure for creating commands for the bot repond;
     - Enabled the intents for the bot work properly (so that it can
    send messages and listen for messages from the server.
    
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

--- src/main/java/com/julioed/thebestbotoftheworld/TheBestBot.java
+++ src/main/java/com/julioed/thebestbotoftheworld/TheBestBot.java
@@ -4,13 +4,14 @@ import com.julioed.thebestbotoftheworld.listeners.EventListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
 
 /**
- * @author julioed <a href=https://www.linkedin.com/in/julio-eduardo-nascimento-da-silva-2576b2173/>...</a>
+ * @author jujulioed <a href=https://www.linkedin.com/in/julio-eduardo-nascimento-da-silva-2576b2173/>...</a>
  */
 public class TheBestBot {
 
@@ -19,6 +20,7 @@ public class TheBestBot {
 
     /**
      * Loads environment variables and builds the bot shard manager.
+     *
      * @throws LoginException occurs when bot token is invalid
      */
 
@@ -30,23 +32,16 @@ public class TheBestBot {
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("Liga das Lendas"));
+
+        // enableIntents - are necessary to give to our bot the permission to do certain actions and access some data
+        // GUILD_MEMBERS - related to users leaving or entering the server, for example, and do some stuff based on that
+        // MESSAGE_CONTENT - related to get a message from the chat and do some stuff based on that
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
         shardManager = builder.build();
 
         // Register listeners
         shardManager.addEventListener(new EventListener());
-    }
-
-    public Dotenv getConfig() {
-        return config;
-    }
-
-    /**
-     * Retrieves the bot shard manager.
-     * @return the ShardManager instance for that bot.
-     */
-
-    public ShardManager getShardManager() {
-        return shardManager;
     }
 
     public static void main(String[] args) {
@@ -56,5 +51,19 @@ public class TheBestBot {
             System.out.println("ERROR: provided bot token invalid");
         }
 
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    /**
+     * Retrieves the bot shard manager.
+     *
+     * @return the ShardManager instance for that bot.
+     */
+
+    public ShardManager getShardManager() {
+        return shardManager;
     }
 }
--- src/main/java/com/julioed/thebestbotoftheworld/listeners/EventListener.java
+++ src/main/java/com/julioed/thebestbotoftheworld/listeners/EventListener.java
@@ -1,15 +1,28 @@
 package com.julioed.thebestbotoftheworld.listeners;
 
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.channels.Channel;
 
+
+/**
+ * Listens for events from discord and responds with our custom code.
+ *
+ * @author jujulioed
+ */
 public class EventListener extends ListenerAdapter {
 
+    /**
+     * Event activates when an emoji reaction is added to a message.
+     */
+
     @Override
-    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
         // Example: ADM reacted to a message with "thumbs up" in the #testing channel
         User user = event.getUser();
         String emoji = event.getReaction().getEmoji().getAsReactionCode();
@@ -17,6 +30,25 @@ public class EventListener extends ListenerAdapter {
 
         String message = user.getName() + " reacted to a message with " + emoji + " in the " + channelMention + " channel!";
         event.getGuild().getDefaultChannel().asStandardGuildMessageChannel().sendMessage(message).queue();
+    }
 
+    /**
+     * Event activates when some user texts "ping".
+     */
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+        if (message.contains("ping")) {
+            event.getChannel().sendMessage("pong").queue();
+        }
+    }
+
+    /**
+     * Event activates when a new user arrives.
+     */
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        String user = event.getUser().getName();
+        String message = "Welcome! " + user + ", you are very welcome in our test server!";
+        event.getGuild().getDefaultChannel().asStandardGuildMessageChannel().sendMessage(message).queue();
     }
 }
