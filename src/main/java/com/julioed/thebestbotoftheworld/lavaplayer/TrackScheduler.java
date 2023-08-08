d20138012abef2f975eaebcc77c7c998a28330ea
juujulio <julioeduardon9@hotmail.com>
2023-08-07 23:52:15 -0300

    Starting to implement the music player feature
     This version is not yet playable
    
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

---
+++ src/main/java/com/julioed/thebestbotoftheworld/lavaplayer/TrackScheduler.java
@@ -0,0 +1,37 @@
+package com.julioed.thebestbotoftheworld.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.*;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TrackScheduler extends AudioEventAdapter {
+
+    public final AudioPlayer audioPlayer;
+    public final BlockingQueue<AudioTrack> queue;
+
+    public TrackScheduler(AudioPlayer audioPlayer) {
+        this.audioPlayer = audioPlayer;
+        this.queue = new LinkedBlockingQueue<>();
+    }
+
+    public void queue(AudioTrack track) {
+        if(!this.audioPlayer.startTrack(track, true)) {
+            this.queue.offer(track);
+        }
+    }
+
+    public void nextTrack() {
+        this.audioPlayer.startTrack(this.queue.poll(), false);
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if(endReason.mayStartNext) {
+
+        }
+    }
+}
